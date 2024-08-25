b41d40e0d0bafec9c5e16251fd61adc6010a8585
louis frederic schefer <lf.schefer@gmail.com>
2024-08-25 17:10:34 +0200

    tailwind setup

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,8 +4,11 @@ import { FormattedMessage } from "react-intl";
 export default function Home() {
 
   return (
-    <h1>
+    <>
+    <h1 className="text-xl">
       <FormattedMessage id="test"/>
     </h1>
+    <button className="py-2 px-3 bg-cyan-500 hover:bg-cyan-600 text-white text-sm font-semibold rounded-md shadow focus:outline-none test">coucou</button>
+    </>
   )
 }

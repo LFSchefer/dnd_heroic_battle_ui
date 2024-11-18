8861aec34d9e09695b5db0c41be8cb65b05d766f
LFSchefer <lf.schefer@gmail.com>
2024-11-18 21:20:50 +0100

    up sign up

---
+++ src/models/user/signUpForm.ts
@@ -0,0 +1,5 @@
+export type signUpForm = {
+    userName: string,
+    email: string,
+    password: string
+}
--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,10 +1,81 @@
+import { SyntheticEvent, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import { useNavigate } from "react-router-dom";
-
+import { signUpForm } from "../../models/user/signUpForm";
 
 export default function SignUp() {
 
+    const [signUpData, setSignUpData] = useState<signUpForm>({
+        userName: '',
+        email: '',
+        password: ''
+    });
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const [errorSignUpForm, setErrorSignUpForm] = useState({
+        userName: false,
+        email: false,
+        password: false,
+        confirmPassword: false
+    });
     const navigate = useNavigate();
+
+    const setField = (input: string, field: string): void => {
+        setSignUpData( prev => {
+            return {
+                ...prev,
+                [field]: input
+            }
+        })
+    }
+
+    const handleChangeConfirmPassword = (value: string): void => {
+        setConfirmPassword(value);
+    }
+
+    const handleSubmit = (event: SyntheticEvent): void => {
+        event.preventDefault();
+        validateInputs();
+    }
+
+    const validateInputs = (): void => {
+        if (signUpData.userName.trim().length < 5 && signUpData.userName.trim().length > 50) {
+            setErrorSignUpForm( prev => {
+                return {
+                    ...prev,
+                    userName: true
+                }
+            })
+        } else if (!signUpData.email) {
+            setErrorSignUpForm( prev => {
+                return {
+                    ...prev,
+                    email: true
+                }
+            })
+        } else if (!signUpData.password) {
+            setErrorSignUpForm( prev => {
+                return {
+                    ...prev,
+                    password: true
+                }
+            })
+        } else if (signUpData.password !== confirmPassword) {
+            setErrorSignUpForm( prev => {
+                return {
+                    ...prev,
+                    confirmPassword: true
+                }
+            })
+        } else {
+            setErrorSignUpForm({
+                userName: false,
+                email: false,
+                password: false,
+                confirmPassword: false
+            })
+            // TODO API CALL
+        }
+    }
 
     const goToSignIn = (): void => {
         navigate("/sign-in");
@@ -20,7 +91,25 @@ export default function SignUp() {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="border rounded-lg border-cyan-800/20 px-8 py-12 space-y-10" noValidate>
+            <form className="border rounded-lg border-cyan-800/20 px-8 py-12 space-y-8" onSubmit={handleSubmit} noValidate>
+                <div>
+                    <div className="flex items-center justify-start">
+                        <label htmlFor="user-name" className="block text-sm/6 font-medium text-gray-900">
+                            < FormattedMessage id="userName"/>
+                        </label>
+                    </div>
+                <div className="mt-2">
+                    <input
+                    id="user-name"
+                    name="user-name"
+                    type="text"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-400 sm:text-sm/6 px-4"
+                    value={signUpData.userName}
+                    onChange={e => setField(e.target.value, "userName")}
+                    />
+                </div>
+                </div>
                 <div>
                     <div className="flex items-center justify-start">
                         <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
@@ -35,10 +124,11 @@ export default function SignUp() {
                     required
                     autoComplete="email"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-400 sm:text-sm/6 px-4"
+                    value={signUpData.email}
+                    onChange={e => setField(e.target.value, "email")}
                     />
                 </div>
                 </div>
-
                 <div>
                 <div className="flex items-center justify-between">
                     <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
@@ -53,6 +143,27 @@ export default function SignUp() {
                     required
                     autoComplete="current-password"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-400 sm:text-sm/6 px-4"
+                    value={signUpData.password}
+                    onChange={e => setField(e.target.value, "password")}
+                    />
+                </div>
+                </div>
+                <div>
+                <div className="flex items-center justify-between">
+                    <label htmlFor="passwordConfirm" className="block text-sm/6 font-medium text-gray-900">
+                        < FormattedMessage id="confirmPassword"/>
+                    </label>
+                </div>
+                <div className="mt-2">
+                    <input
+                    id="passwordConfirm"
+                    name="passwordConfirm"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-400 sm:text-sm/6 px-4"
+                    value={confirmPassword}
+                    onChange={e => handleChangeConfirmPassword(e.target.value)}
                     />
                 </div>
                 </div>

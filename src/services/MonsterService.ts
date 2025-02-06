f431a58b337b8416d2b915b1930467d7f5218b12
LFSchefer <lf.schefer@gmail.com>
2025-02-06 22:59:26 +0100

    refacto

--- src/services/MonsterService.ts
+++ src/services/MonsterService.ts
@@ -2,7 +2,8 @@ import axiosClient from "./AxiosClient"
 
 export default class MonsterService {
    
-   static createBattleMonster = async(monsterId: number, name: string, currentHitPoints: number, maxHitPoints: number, battleId: number): Promise<void> => {
+   static createBattleMonster = async(monsterId: number, name: string, currentHitPoints: number,
+     maxHitPoints: number, battleId: number): Promise<void> => {
         try {
             const data = {monsterId, name, currentHitPoints, maxHitPoints, battleId}
             await axiosClient.post("/monsters", data );

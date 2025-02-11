a946ecd952251ce3011f0feb14b9ecdc055ce691
LFSchefer <lf.schefer@gmail.com>
2025-02-11 12:34:42 +0100

    add presentation text

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -51,6 +51,23 @@ export default function Home() {
         </button>
         }
       </div>
+      <div className="presentation-text bg-blue-200 w-3/6 rounded-lg shadow-md py-4 px-4 mx-auto my-5 text-left">
+        <div>
+          <FormattedMessage id="presentationText"/>
+        </div>
+        <div className="mt-2">
+          <FormattedMessage id="presentationTextDetail1"/>
+        </div>
+        <div className="mt-2">
+          <FormattedMessage id="presentationTextDetail2"/>
+        </div>
+        <div className="mt-2">
+          <FormattedMessage id="presentationTextDetail3"/>
+        </div>
+        <div className="mt-2">
+          <FormattedMessage id="presentationTextDetail4"/>
+        </div>
+      </div>
       < DiceRoller/>
     </>
   )

593763d2190739ee12c75987793c47adf3d50740
LFSchefer <lf.schefer@gmail.com>
2026-01-09 18:45:40 +0100

    add auto open at start & bg for 404

--- src/pages/notfound/NotFound.tsx
+++ src/pages/notfound/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound: FC = () => {
   }
 
   return (
-<main className="grid place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-screen">
+<main className="grid place-items-center px-6 py-24 sm:py-32 lg:px-8 h-screen">
   <div className="text-center">
     <p className="text-4xl font-semibold text-cyan-600">404</p>
     <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: { port: 3000},
+  server: { 
+    port: 3000,
+    open: './index.html'
+  },
   plugins: [
     react({
       babel: {

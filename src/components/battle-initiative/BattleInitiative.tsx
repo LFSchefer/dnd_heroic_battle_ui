f60d5df66198b10445ceb8ab07bebed752330818
LFSchefer <lf.schefer@gmail.com>
2026-06-22 19:35:13 +0200

    fix update value of initiative

--- src/components/battle-initiative/BattleInitiative.tsx
+++ src/components/battle-initiative/BattleInitiative.tsx
@@ -6,53 +6,65 @@ import InitiativeCard from "../initiative-card/InitiativeCard";
 import { FormattedMessage } from "react-intl";
 
 type Props = {
-    updateBattle:() => void
-}
+  updateBattle: () => void;
+};
 
 const BattleInitiative: FC<Props> = (props: Props) => {
+  const { updateBattle } = props;
 
-    const {updateBattle} = props;
+  const params = useParams();
+  const battleId = +params?.battleId!;
 
-    const params = useParams();
-    const battleId = +params?.battleId!;
+  const [monsterInitiativePreview, setMonsterInitiativePreview] = useState<
+    MonsterInitiative[]
+  >([]);
 
-    const [ monsterInitiativePreview, setMonsterInitiativePreview] = useState<MonsterInitiative[]>([])
-    
+  const update = useCallback(() => {
+    updateBattle();
+    fetchInitiatives(battleId);
+  }, [updateBattle]);
 
-    const fetchInitiatives = useCallback(async (battleId: number) => {
-        const response = await MonsterService.getMonstersInitiativesFromBattle(battleId);
-        setMonsterInitiativePreview(response);
-    },[])
+  const fetchInitiatives = useCallback(
+    async (battleId: number) => {
+      const response =
+        await MonsterService.getMonstersInitiativesFromBattle(battleId);
+      setMonsterInitiativePreview(response);
+    },
+    [],
+  );
 
-    const update = useCallback(() => {
-        updateBattle();
-    },[updateBattle])
+  const handleCalculateAll = useCallback(async (): Promise<void> => {
+    await MonsterService.calculateAllInitiative(monsterInitiativePreview);
+    updateBattle();
+  }, [monsterInitiativePreview, updateBattle]);
 
-    const handleCalculateAll = useCallback(async(): Promise<void> => {
-        await MonsterService.calculateAllInitiative(monsterInitiativePreview);
-        updateBattle();
-    },[monsterInitiativePreview, updateBattle])
+  useEffect(() => {
+    if (battleId) {
+        fetchInitiatives(battleId);
+    }
+  }, [battleId, fetchInitiatives]);
 
-    useEffect(() => {
-        if (battleId) {
-            fetchInitiatives(battleId);
-        }
-    },[battleId, fetchInitiatives, update])
-
-    return (
-        <div className="calculate-initiative-container mt-4">
-            {monsterInitiativePreview.filter( monster => monster.initiative === null).length > 0 &&
-                <button className="dnd-btn" onClick={handleCalculateAll}><FormattedMessage id="calculateAllInititative"/></button>
-            }
-            { monsterInitiativePreview.sort((a,b) => a.id - b.id).map((monster) => {
-                return <InitiativeCard
-                key={monster.id}
-                monster={monster}
-                update={update}
-                />
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="calculate-initiative-container mt-4">
+      {monsterInitiativePreview.filter((monster) => monster.initiative === null)
+        .length > 0 && (
+        <button className="dnd-btn" onClick={handleCalculateAll}>
+          <FormattedMessage id="calculateAllInititative" />
+        </button>
+      )}
+      {monsterInitiativePreview
+        .sort((a, b) => a.id - b.id)
+        .map((monster) => {
+          return (
+            <InitiativeCard
+              key={monster.id}
+              monster={monster}
+              update={update}
+            />
+          );
+        })}
+    </div>
+  );
+};
 
 export default BattleInitiative;
